6c4468219bf60f925d79fbe136cca5d2d5d0d3ac
CarlosPD1997 <carlospiza08@gmail.com>
2024-06-29 00:36:30 -0600

    requisicion

--- api/PlataformaUsuario/Index/views.py
+++ api/PlataformaUsuario/Index/views.py
@@ -2,15 +2,15 @@ from tkinter import Image
 from rest_framework.views import APIView
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-# from reportlab.lib.pagesizes import letter, landscape
-# from reportlab.pdfgen import canvas
-# from reportlab.lib.utils import ImageReader
-# from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
-# from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.pdfgen import canvas
+from reportlab.lib.utils import ImageReader
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
+from reportlab.lib import colors
 from datetime import datetime
 from django.contrib.staticfiles import finders
-# from reportlab.lib.units import inch
-# from reportlab.platypus import Frame
+from reportlab.lib.units import inch
+from reportlab.platypus import Frame
 from api.models import requisicion
 from django.contrib import messages
 import io
@@ -69,125 +69,125 @@ class Index(APIView):
     
     
     
-    # def post(self,request):
-    #     try:
-    #         user = request.user
-    #         grupo = request.POST.get('grupo')
-    #         inicio = request.POST.get('inicio')
-    #         fecha = request.POST.get('fecha')
-    #         fin = request.POST.get('fin')
-    #         docente = request.POST.get('docente')
-    #         asignatura = request.POST.get('asignatura')
-    #         taller = request.POST.get('taller')
+    def post(self,request):
+        try:
+            user = request.user
+            grupo = request.POST.get('grupo')
+            inicio = request.POST.get('inicio')
+            fecha = request.POST.get('fecha')
+            fin = request.POST.get('fin')
+            docente = request.POST.get('docente')
+            asignatura = request.POST.get('asignatura')
+            taller = request.POST.get('taller')
 
-    #         utensilios = []
-    #         for key, value in request.POST.items():
-    #             if key.startswith('utensilios'):
-    #                 parts = key.split('[')
-    #                 index = int(parts[1][:-1])
-    #                 field = parts[2][:-1]
-    #                 while len(utensilios) <= index:
-    #                     utensilios.append({})
-    #                 utensilios[index][field] = value
-    #         utensilios = [[u['nombre'], u['cantidad']] for u in utensilios if 'nombre' in u and 'cantidad' in u]
-    #         print(utensilios)
-    #         fecha_actual = datetime.now().strftime('%Y-%m-%d')
+            utensilios = []
+            for key, value in request.POST.items():
+                if key.startswith('utensilios'):
+                    parts = key.split('[')
+                    index = int(parts[1][:-1])
+                    field = parts[2][:-1]
+                    while len(utensilios) <= index:
+                        utensilios.append({})
+                    utensilios[index][field] = value
+            utensilios = [[u['nombre'], u['cantidad']] for u in utensilios if 'nombre' in u and 'cantidad' in u]
+            print(utensilios)
+            fecha_actual = datetime.now().strftime('%Y-%m-%d')
 
-    #         # Crear el PDF en memoria
-    #         buffer = io.BytesIO()
-    #         doc = SimpleDocTemplate(buffer, pagesize=landscape(letter))
-    #         elements = []
+            # Crear el PDF en memoria
+            buffer = io.BytesIO()
+            doc = SimpleDocTemplate(buffer, pagesize=landscape(letter))
+            elements = []
 
-    #         # Título y datos del encabezado
-    #         title = "2024. Año del Bicentenario de la Erección del Estado Libre y Soberano de México"
-    #         header_data = [
-    #             ["PRÁCTICAS DE LOS TALLERES DE LA LICENCIATURA EN GASTRONOMÍA "],
-    #             ["FECHA:", fecha_actual, "GRUPO:", grupo],
-    #             ["HORA DE INICIO:", inicio, "HORA DE TÉRMINO:", fin],
-    #             ["DOCENTE:", docente, "ASIGNATURA:", asignatura],
-    #             ["TALLER:", taller, "", ""]
-    #         ]
+            # Título y datos del encabezado
+            title = "2024. Año del Bicentenario de la Erección del Estado Libre y Soberano de México"
+            header_data = [
+                ["PRÁCTICAS DE LOS TALLERES DE LA LICENCIATURA EN GASTRONOMÍA "],
+                ["FECHA:", fecha_actual, "GRUPO:", grupo],
+                ["HORA DE INICIO:", inicio, "HORA DE TÉRMINO:", fin],
+                ["DOCENTE:", docente, "ASIGNATURA:", asignatura],
+                ["TALLER:", taller, "", ""]
+            ]
 
-    #         header_table = Table(header_data, colWidths=[150, 200, 150, 200])
-    #         header_table.setStyle(TableStyle([
-    #             ('ALIGN', (0, 0), (-1, 0), 'CENTER'),
-    #             ('SPAN', (0, 0), (-1, 0)),
-    #             ('BACKGROUND', (0, 0), (0, 0), colors.grey),
-    #             ('TEXTCOLOR', (0, 0), (0, 0), colors.whitesmoke),
-    #             ('ALIGN', (0, 1), (-1, -1), 'LEFT'),
-    #             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-    #             ('FONTSIZE', (0, 0), (-1, 0), 10),
-    #             ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-    #             ('GRID', (0, 1), (-1, -1), 1, colors.black),
-    #         ]))
-    #         elements.append(header_table)
-    #         elements.append(Spacer(1, 20))
+            header_table = Table(header_data, colWidths=[150, 200, 150, 200])
+            header_table.setStyle(TableStyle([
+                ('ALIGN', (0, 0), (-1, 0), 'CENTER'),
+                ('SPAN', (0, 0), (-1, 0)),
+                ('BACKGROUND', (0, 0), (0, 0), colors.grey),
+                ('TEXTCOLOR', (0, 0), (0, 0), colors.whitesmoke),
+                ('ALIGN', (0, 1), (-1, -1), 'LEFT'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('FONTSIZE', (0, 0), (-1, 0), 10),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('GRID', (0, 1), (-1, -1), 1, colors.black),
+            ]))
+            elements.append(header_table)
+            elements.append(Spacer(1, 20))
 
-    #         # Agregar la tabla de utensilios
-    #         utensilios_data = [
-    #             ["NOMBRE \n DE \n UTENSILIOS", "CANTIDAD \n REQUERIDA \n  POR EL \n ESTUDIANTE", "CANTIDAD \n ENTREGADA \n POR EL ALMACÉN", "CANTIDAD \n RECIBIDA \n POR \n ENCARGADO \n DEL \n ALMACÉN Y \n ENCARGADO \n DE ÁREA"],
-    #         ]
-    #         for utensilio in utensilios:
-    #             utensilios_data.append([utensilio[0], utensilio[1], "", ""])
+            # Agregar la tabla de utensilios
+            utensilios_data = [
+                ["NOMBRE \n DE \n UTENSILIOS", "CANTIDAD \n REQUERIDA \n  POR EL \n ESTUDIANTE", "CANTIDAD \n ENTREGADA \n POR EL ALMACÉN", "CANTIDAD \n RECIBIDA \n POR \n ENCARGADO \n DEL \n ALMACÉN Y \n ENCARGADO \n DE ÁREA"],
+            ]
+            for utensilio in utensilios:
+                utensilios_data.append([utensilio[0], utensilio[1], "", ""])
 
-    #         utensilios_table = Table(utensilios_data, colWidths=[150, 100, 100, 100])
-    #         utensilios_table.setStyle(TableStyle([
-    #             ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-    #             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-    #             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-    #             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-    #             ('FONTSIZE', (0, 0), (-1, 0), 10),
-    #             ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-    #             ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-    #             ('GRID', (0, 0), (-1, -1), 1, colors.black),
-    #         ]))
-    #         elements.append(utensilios_table)
+            utensilios_table = Table(utensilios_data, colWidths=[150, 100, 100, 100])
+            utensilios_table.setStyle(TableStyle([
+                ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('FONTSIZE', (0, 0), (-1, 0), 10),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ]))
+            elements.append(utensilios_table)
 
-    #         # Añadir la tabla de firma
-    #         firma_data = [
-    #             ["__________________ \n FIRMA DE ALMACÉN"]
-    #         ]
-    #         firma_table = Table(firma_data, colWidths=[200], rowHeights=[60])
-    #         firma_table.setStyle(TableStyle([
-    #             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-    #             ('FONTNAME', (0, 0), (-1, -1), 'Helvetica'),
-    #             ('FONTSIZE', (0, 0), (-1, -1), 10),
-    #             ('GRID', (0, 0), (-1, -1), 1, colors.black),
-    #         ]))
-    #         elements.append(Spacer(0, 20))
-    #         elements.append(firma_table)
+            # Añadir la tabla de firma
+            firma_data = [
+                ["__________________ \n FIRMA DE ALMACÉN"]
+            ]
+            firma_table = Table(firma_data, colWidths=[200], rowHeights=[60])
+            firma_table.setStyle(TableStyle([
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, -1), 'Helvetica'),
+                ('FONTSIZE', (0, 0), (-1, -1), 10),
+                ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ]))
+            elements.append(Spacer(0, 20))
+            elements.append(firma_table)
 
-    #         doc.build(elements)
+            doc.build(elements)
 
-    #         buffer.seek(0)
-    #         pdf_content = buffer.getvalue()
+            buffer.seek(0)
+            pdf_content = buffer.getvalue()
 
-    #         # Guardar el PDF en un archivo temporal
-    #         temp_file = ContentFile(pdf_content)
-    #         temp_file_name = f"Requisicion_{fecha_actual}.pdf"
-    #         codigo_unico = generar_codigo_unico()  # Asegúrate de que esta función está definida
+            # Guardar el PDF en un archivo temporal
+            temp_file = ContentFile(pdf_content)
+            temp_file_name = f"Requisicion_{fecha_actual}.pdf"
+            codigo_unico = generar_codigo_unico()  # Asegúrate de que esta función está definida
 
-    #         # Crear el objeto Requisicion y guardar el PDF en el campo FileField
-    #         oficio = requisicion.objects.create(
-    #             codigo=codigo_unico,
-    #             hora_inicio=inicio,
-    #             hora_fin=fin,
-    #             grupo=grupo,
-    #             pdf=temp_file,
-    #             created_date=fecha_actual,
-    #             docente=docente
-    #         )
-    #         oficio.users.add(user)
-    #         # Guardar el archivo en el FileField
-    #         oficio.pdf.save(temp_file_name, temp_file)
+            # Crear el objeto Requisicion y guardar el PDF en el campo FileField
+            oficio = requisicion.objects.create(
+                codigo=codigo_unico,
+                hora_inicio=inicio,
+                hora_fin=fin,
+                grupo=grupo,
+                pdf=temp_file,
+                created_date=fecha_actual,
+                docente=docente
+            )
+            oficio.users.add(user)
+            # Guardar el archivo en el FileField
+            oficio.pdf.save(temp_file_name, temp_file)
 
-    #         # Retornar el archivo PDF como respuesta
-    #         response = HttpResponse(pdf_content, content_type='application/pdf')
-    #         response['Content-Disposition'] = f'attachment; filename={temp_file_name}'
-    #         return response
+            # Retornar el archivo PDF como respuesta
+            response = HttpResponse(pdf_content, content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename={temp_file_name}'
+            return response
 
-    #     except Exception as e:
-    #         print(f'Error al generar el PDF: {str(e)}')
-    #         messages.error(request, f'Error al generar el PDF: {str(e)}')
-    #         return render(request, '404.html')
+        except Exception as e:
+            print(f'Error al generar el PDF: {str(e)}')
+            messages.error(request, f'Error al generar el PDF: {str(e)}')
+            return render(request, '404.html')
     
